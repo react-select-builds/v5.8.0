cee9b3a974ad47c654601ef2620334cceb5c7fab
Artur Klesun <ArturKlesun@gmail.com>
2024-09-12 15:11:32 +0300

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -3,17 +3,17 @@ import epeg from "esbuild-plugin-external-global";
 
 await esbuild.build({
     entryPoints: ['src/main.jsx'],
-	bundle: true,
-	minify: true,
-	sourcemap: true,
-	outfile: "dist/react-select.esm.min.js",
-	format: "esm",
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: "dist/react-select.esm.min.js",
+    format: "esm",
     target: ['es6'],
     external: ['react', 'react-dom'],
 	plugins: [
 	    epeg.externalGlobalPlugin({
             'react': 'window.React',
             'react-dom': 'window.ReactDOM',
-		}),
-	],
+        }),
+    ],
 });
